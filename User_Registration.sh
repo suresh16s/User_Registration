776a21c81dbde010d830e2b4a66efe6a115b15f2
suresh16s <sureshthite48@gmail.com>
2020-07-14 20:56:42 +0530

    Last Name

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,9 +1,20 @@
-
+#validate First name:
 read -p "Enter The FirstName:" First
-PAT="^[A-Z]{1}[a-z]{2,}$";
+PAT="^[A-Z]{1}[a-z]{2,}$"
 if [[ $First =~ $PAT ]]
 then
 	echo "FirstName Is Valid";
 else
-	echo "FirstName Is Not Valid";
+	echo "Please enter Valid Name:";
 fi
+
+#validate last name:
+read -p "Enter The LastName:"  last
+PAT1="^[A-Z]{1}[a-z]{2,}$";
+if [[ $last =~ $PAT1 ]]
+then
+	echo "LastName Is Valid:";
+else
+	echo "Please enter valid last name:";
+fi
+
