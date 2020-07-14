d3c9e4f2883514ada1a8cc6d7b834558d73dcd30
suresh16s <sureshthite48@gmail.com>
2020-07-14 21:07:12 +0530

    Validate Phone No.

--- User_Registration.sh
+++ User_Registration.sh
@@ -27,3 +27,14 @@ then
 else
 	echo "Please enter the valid Email:"
 fi
+
+
+#validate Phone Number
+read -p "Enter the phone No:" PhoneNo
+PAT3="^(\+[0-9]{2}\s)[0-9]{10}$"
+if [[ $PhoneNo =~ $PAT3 ]]
+then
+	echo "Phone Number Is Valid";
+else
+	echo "Phone Number Is Not Valid";
+fi
